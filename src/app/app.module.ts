2a31fc6bbb7703f2d3f012b7ef93d2515c618b4c
Andy Yeung <andyyeung122@gmail.com>
2022-03-20 19:23:18 -0400

    create pure task list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { TaskModule } from './components/task/task.module';
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+
 import { AppComponent } from './app.component';
+import { PureInboxScreenComponent } from './components/inbox-screen/pure-inbox-screen.component';
+import { InboxScreenComponent } from './components/inbox-screen/inbox-screen.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, InboxScreenComponent, PureInboxScreenComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TaskModule,
+    NgxsModule.forRoot([]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
